aca77cdb41598c4c158a44ab58d1c32ee9196b90
AKSHAYCHIDRE <achidre.ivision@gmail.com>
2020-01-28 12:25:44 +0530

    updated cogress website pages

---
+++ js/slick.js
@@ -0,0 +1,14 @@
+$('.slideshow').slick({
+  infinite: true,
+  autoplay: true,
+  dots: true,
+  arrows: true,
+  autoplaySpeed: 4000
+});
+
+
+//  animation JS here
+
+AOS.init({
+  duration: 1500,
+})
